c9df6bc0d7c204cbc5d125f13b1fe5ceaa5c79b3
Gulshan <gulshan.kumar.dsa@gmail.com>
2025-05-29 14:47:53 +0300

    Added Login and logout redirect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter   as Router, Routes, Route, Navigate  } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import './App.css';
+import { AuthProvider, useAuth } from './context/AuthContext';
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated() ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
+   
     <Router>
+       <AuthProvider>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+        </AuthProvider>
     </Router>
+
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import  '../assets/styles/Navbar.css'
-
+import { useAuth } from '../context/AuthContext'
 
 const Navbar = () => {
+  const { isAuthenticated, logout } = useAuth();
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -32,6 +33,16 @@ const Navbar = () => {
               Contact
             </Link>
           </li>
+            {isAuthenticated() ? (
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+              <button onClick={logout}>Logout</button>
+            </li>
+          ) : (
+            <li>
+            <Link to="/login">Login</Link>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
